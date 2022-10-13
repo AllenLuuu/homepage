835d4aa415cfec2ec77423842836b5e1ea936413
AllenLu <allenluuu@foxmail.com>
2022-10-14 00:25:42 +0800

    feat: change Sentence display

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./style.css";
+import "vfonts/FiraSans.css";
+import "vfonts/FiraCode.css";
 import App from "./App.vue";
 
 const pinia = createPinia();
