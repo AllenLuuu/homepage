f35785b6a7d13f4316a0dc92ccae030e3d7492bc
AllenLu <allenluuu@foxmail.com>
2022-10-19 00:37:20 +0800

    feat: change & add some tags

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -4,14 +4,17 @@
     <NTag type="success">浙大计科</NTag>
     <NTag type="info">水瓶座</NTag>
     <NTag type="primary">INFP</NTag>
+    <NTag type="warning">赫奇帕奇肄业生</NTag>
     <NTag type="error">梅西球迷</NTag>
-    <NTag type="warning">赫奇帕奇留级生</NTag>
+    <NTag type="success">只会踢边路</NTag>
+    <NTag type="warning">喜欢芒果制品</NTag>
     <NTag type="primary">初级占卜师</NTag>
-    <NTag type="error">爵士乐爱好者</NTag>
-    <NTag type="warning">O麦金不定期持有者</NTag>
-    <NTag type="primary">张爱玲狂热粉丝</NTag>
     <NTag type="success">睡觉爱好者</NTag>
+    <NTag type="warning">O麦金不定期持有者</NTag>
+    <NTag type="primary">张爱玲忠实粉丝</NTag>
+    <NTag type="error">喜欢王菲</NTag>
     <NTag type="warning">快门民工</NTag>
     <NTag type="info">一台牛顿反射式望远镜的主人</NTag>
+    <NTag type="default">无知的人</NTag>
   </NSpace>
 </template>
